5f7458dbd77e2bd0578e65d1576ba0ba148026c0
RobinLind <robinlind@Robins-MacBook-Pro.local>
2017-05-11 12:26:39 +0200

    test for om php kode kan kjøre

--- reg-smaetch/test.php
+++ reg-smaetch/test.php
@@ -1,5 +1,22 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
 
-echo '<pre>';
+<title>New HMTL document by NewJect</title>
 
-print_r($_POST);
+</head>
+<body>
+
+<?php $nums = [1,2,3]; ?>
+<ul>
+<?php foreach ($nums as $num) { ?>
+ <li>nr <?= $num ?></li>
+<?php } ?>
+</ul> 
+</body>
+</html>
