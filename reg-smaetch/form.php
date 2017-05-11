83b189789626c08ab84704dcdc5c8361d185052e
RobinLind <robinlind@Robins-MacBook-Pro.local>
2017-05-11 10:55:27 +0200

    komentert ut bildeupload

--- reg-smaetch/form.php
+++ reg-smaetch/form.php
@@ -21,8 +21,8 @@ if(isset($_POST["studor"])){
 $target_file = $target_dir . basename($_FILES["bilde"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
-if(isset($_POST["studor"])) {
+// Check if image file is a actual image or fake image 
+/*if(isset($_POST["studor"])) {
     $check = getimagesize($_FILES["bilde"]["tmp_name"]);
     if($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
@@ -60,7 +60,7 @@ if ($uploadOk == 0) {
 }
  $sql = "INSERT INTO Studor (fornavn, etternavn, studentNr, email, bilde, passord) VALUES ('$fornavn', '$etternavn', '$studentNR', '$email', '', '$password')"; 
 } else if (isset($_POST["student"])){
-
+*/
    echo "Form 1 have been submitted";
 // attempt insert query execution
 $sql = "INSERT INTO NyStudent (Studentnummer, fornavn, etternavn, StudentMail, Passord) VALUES ('$studentNR', '$fornavn', '$etternavn', '$email' '$password')";
