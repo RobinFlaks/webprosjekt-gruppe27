7110e95411f648f51372af7698514b8a85e0074c
nicolineaa <nicolineaarup@gmail.com>
2017-05-27 21:45:25 +0200

    Endret Brenneriveien html+Css
    Endret html/css for Brenneriveien, samt ny logo og menybar

---
+++ skolesider/reg-smaetch/test.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>New HMTL document by NewJect</title>
+
+</head>
+<body>
+
+<?php $nums = [1,2,3]; ?>
+<ul>
+<?php foreach ($nums as $num) { ?>
+ <li>nr <?= $num ?></li>
+<?php } ?>
+</ul> 
+</body>
+</html>
